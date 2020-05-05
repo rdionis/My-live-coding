c385dcc81a9d79c9a958544cc06269a5905e6846
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-05-05 18:41:15 +0200

    updating my-live-coding

--- 5.May2020/04-05/src/scripts/index.js
+++ 5.May2020/04-05/src/scripts/index.js
@@ -13,34 +13,20 @@
 //JavaScript Object Notation (JSON)
 // It's a file format, and data interchange format, that uses human-readable text to store and transmit data objects
 
-import { data } from "./data.js";
-let parsedData = JSON.parse(data);
+import { data } from "./data.js"; //WHAT I WANT TO IMPORT AND FROM WHERE
 
-const directorHtml = document.getElementById("director"),
-  imgHtml = document.querySelector("img"),
-  cardBody = document.querySelector(".card__body"),
-  toHover = document.querySelector(".toHover"),
-  section = document.querySelector(".list");
+let parsedData = JSON.parse(data); // I AM CONVERTING JSON INTO AN OBJECT THAT JS WILL UNDERSTAND
+const section = document.querySelector("#bread");
+console.log(parsedData);
 
 parsedData.forEach((obj) => {
-  let { id, title } = obj;
-  let card = `<div id = ${id}>${title}</div>`;
+  console.log(obj);
+  let { id, title, ingredients, image } = obj; //EXTRACTING KEYS FROM THE OBJECT
+  let card = ` <div id=${id}>
+  <h2> ${title}</h2>
+  <img src=${image} alt=${title}>
+  <h6> ${ingredients}</h6>
+  </div>
+  `;
   section.innerHTML += card;
 });
-let listItems = document.querySelector(".list").querySelectorAll("div");
-listItems.forEach((item) => {
-  item.addEventListener("click", changes);
-});
-
-function changes() {
-  listItems.forEach((item) => {
-    this.style.backgroundColor = "#082e4d";
-    this.style.transform = "translateX(2rem)";
-    item.removeAttribute("style");
-    toHover.style.opacity = "1";
-  });
-  let data = parsedData.find((data) => data.id == this.id);
-  directorHtml.innerHTML = data.director;
-  cardBody.innerHTML = data.description;
-  imgHtml.src = data.image;
-}
