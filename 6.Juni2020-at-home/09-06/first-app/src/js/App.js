23e1614ccd2ae216c2b93d4cb314bb4a84c2428b
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-06-09 13:51:11 +0200

    updating my-live coding

---
+++ 6.Juni2020-at-home/09-06/first-app/src/js/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p>Hi, I am React</p>
+
+      <img src="./img/sky.jpg" alt="sky" />
+    </div>
+  );
+}
+
+export default App;
