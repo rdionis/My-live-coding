ef0dcbd52f4e662eda5df5544b055aa89d8e3102
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-06-10 13:37:44 +0200

    updating 10-06

---
+++ 6.June2020-at-home/10-06/src/js/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Text from "./Text";
+
+const App = () => {
+  //you always need App
+  return (
+    <div>
+      <h1>Hey, I am React</h1>
+      <Text //I can pass stuff here
+      />
+      <Text />
+    </div>
+  );
+};
+
+export default App;
---
+++ 6.June2020-at-home/10-06/src/js/Text.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    // every time you use return in React, you need a wrapper
+    <div>
+      <h5>Hey, I am component</h5>
+    </div>
+  );
+};
+
+export default Text;
