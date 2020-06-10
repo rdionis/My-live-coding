0633fb96734f607a87fbce4fe447f702659f2f4d
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-06-10 15:25:32 +0200

    finishing doover

--- 6.June2020-at-home/doover/src/index.js
+++ 6.June2020-at-home/doover/src/index.js
@@ -2,3 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./scss/main.scss";
 import App from "./js/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- 6.June2020-at-home/doover/src/js/App.js
+++ 6.June2020-at-home/doover/src/js/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Text from "./Text";
 
-const App = () =>{
-    return(
-        <div>
-            <h1>Hey, I am React App doover</h1>
-            <Text "oh, my"/>
-            <Text/>
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      <h1>Hey, I am React App doover</h1>
+      <Text />
+      <Text />
+      <img src="./img/purple_stuff.jpg" alt="purple stuff" />
+    </div>
+  );
 };
 
 export default App;
