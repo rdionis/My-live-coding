6bc5bbe45ce947896efb21a0ea8fb3036416610a
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-06-16 09:11:25 +0200

    updadting my-live-coding

---
+++ 6.June2020-at-home/16-06/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hi I am React</h1>
+      </React.Fragment>
+    );
+  }
+}
